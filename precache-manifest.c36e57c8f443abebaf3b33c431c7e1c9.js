e22137404daaec440be0a138333ab54802298050
KenWronka <ken.wronka@gmail.com>
2020-01-27 20:05:43 -0500

    Updates

---
+++ precache-manifest.c36e57c8f443abebaf3b33c431c7e1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787047a61dc8988d679e6b350223817e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "34f77b95b2ab21b23ee4",
+    "url": "/monsters-rolodex/static/css/main.951b7a8c.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34f77b95b2ab21b23ee4",
+    "url": "/monsters-rolodex/static/js/main.847046b8.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
